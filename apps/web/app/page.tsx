1732401f487c67cb2f775627695c9c9bde5bd8b8
Graham <guanguangqiang@6677tech.com>
2025-04-11 10:12:31 +0800

    feat: update css

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,7 +2,7 @@ import InputForm from '../componnets/input-form';
 import PostList from '../componnets/post-list';
 export default function Home() {
   return (
-    <main className='space-y-3 flex items-center flex-col'>
+    <main className='space-y-5 flex items-center flex-col'>
       <InputForm />
       <PostList />
     </main>
