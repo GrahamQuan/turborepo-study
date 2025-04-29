cf5b78d2efdde4f66eb02734642dbfd3332bc081
Graham <guanguangqiang@6677tech.com>
2025-04-29 10:38:11 +0800

    fix: schema

--- packages/db/src/schema/post.ts
+++ packages/db/src/schema/post.ts
@@ -1,17 +1,11 @@
-import { index, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
+import { pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
 
-export const posts = pgTable(
-  'posts',
-  {
-    id: serial('id').primaryKey(),
-    title: text('title').notNull(),
-    content: text('content').notNull(),
-    createdAt: timestamp('created_at').notNull().defaultNow(),
-    updatedAt: timestamp('updated_at').notNull().defaultNow(),
-    deletedAt: timestamp('deleted_at'),
-    archivedAt: timestamp('archived_at'),
-  },
-  (t) => ({
-    idIndex: index('id_idx').using('btree', t.id),
-  })
-);
+export const posts = pgTable('posts', {
+  id: serial('id').primaryKey(),
+  title: text('title').notNull(),
+  content: text('content').notNull(),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow(),
+  deletedAt: timestamp('deleted_at'),
+  archivedAt: timestamp('archived_at'),
+});
